1b9f00c9320751a3e33ac30dfd763671528cda54
kaskjfd <105328163+kaskjfd@users.noreply.github.com>
2022-11-29 20:21:52 +0900

    Update game3.java

--- game3.java
+++ game3.java
@@ -1,4 +1,7 @@
-// 점수 시스템(마신 물 개수 계속 출력하게끔)
+// 점수 시스템(현재 점수 출력하게끔)
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.*;
 class   game3 {
     Random rand = new Random();
@@ -33,19 +36,8 @@ class   game3 {
         System.out.print("안녕하세요! 독을 피해 물을 마셔라! 게임에 오신 것을 환영합니다!");
         System.out.println(poisonlength + " " + poisonheight);
         nextment = Scan.nextLine();
-        System.out.println("설명이 필요하십니까?\nyes or no");
-        while (!explainaion.equals("yes") || !explainaion.equals("no")) {
-            explainaion = Scan.nextLine();
-            if (explainaion.equals("yes")) {
-                System.out.println("설명 ~~~~~~~~");
-                nextment = Scan.nextLine();
-                break;
-            } else if (explainaion.equals("no")) {
-                break;
-            }
-        }
-        System.out.println("게임을 시작합니다!");
-        nextment = Scan.nextLine();
+        System.out.println("설명이 필요하시다면 GUI창의 yes을 눌러주세요!");
+
     }
 
     void player1select() {
@@ -57,6 +49,9 @@ class   game3 {
             nowbox();
             player1masilmulselect();
             player1false();
+            if (breakwhile == 1) {
+                return;
+            }
             while (player1length > 9 || player1length < 1 || player1height > 9 || player1height < 1 || box[player1length-1][player1height-1].equals("\uD83D\uDEAB")) {
                 player1notrange();
                 nextment = Scan.nextLine();
@@ -93,7 +88,6 @@ class   game3 {
         nextment = Scan.nextLine();
         box[player1length-1][player1height-1] = "\uD83D\uDEAB";
         if (playercnt == 5) {
-            player1true();
             System.out.println("player1은 독을 마시지 않았네요!");
             nextment = Scan.nextLine();
             System.out.println("player2가 마실 차례에요!");
@@ -119,6 +113,8 @@ class   game3 {
             System.out.println("player2 승!");
             breakwhile = 1;
             player2gamescore += player2score;
+            player1score = 0;
+            player2score = 0;
             gamescoreprint();
         }
         if (playercnt != 1) {
@@ -163,6 +159,9 @@ class   game3 {
             nowbox();
             player2masilmulselect();
             player2false();
+            if (breakwhile == 1) {
+                return;
+            }
             while (player2length > 9 || player2length < 1 || player2height > 9 || player2height < 1 || box[player2length-1][player2height-1].equals("\uD83D\uDEAB")) {
                 player2notrange();
                 nextment = Scan.nextLine();
@@ -199,8 +198,7 @@ class   game3 {
         nextment = Scan.nextLine();
         box[player2length-1][player2height-1] = "\uD83D\uDEAB";
         if (playercnt == 5) {
-            player2true();
-            System.out.println("player1은 독을 마시지 않았네요!");
+            System.out.println("player2은 독을 마시지 않았네요!");
             nextment = Scan.nextLine();
             System.out.println("player1이 마실 차례에요!");
             nextment = Scan.nextLine();
@@ -213,9 +211,12 @@ class   game3 {
             System.out.println("player1 승!");
             breakwhile = 1;
             player1gamescore += player1score;
+            player1score = 0;
+            player2score = 0;
+            gamescoreprint();
         }
         if (playercnt != 1) {
-            if (player1length == 99 && player1height == 99) {
+            if (player2length == 99 && player2height == 99) {
                 break1 = 1;
                 System.out.println("player2는 독을 마시지 않았네요!");
                 nextment = Scan.nextLine();
@@ -257,13 +258,44 @@ class   game3 {
     }
 }
 
+class Detail2 extends JFrame {
+    Detail2() {
+        setTitle("화살 게임");
+        setSize(200, 100);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JPanel p = new JPanel();
+        JLabel l = new JLabel("설명이 필요하십니까?");
+        JButton b1 = new JButton("Yes");
+        JButton b2 = new JButton("No");
+        p.add(l);
+        p.add(b1);
+        p.add(b2);
+        add(p);
+        ActionListener l1 = new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                System.out.println();
+                System.out.println();
+                System.out.println("사과, 바나나, 오렌지가 그려져 있는 과녁이 랜덤하게 10번 나옵니다.\n플레이어는 과녁에 그려져 있는 그림과 같은 모양의 화살을 발사해야 합니다.\n과녁을 모두 부순 시간을 측정하여 더 빠른 시간 내에 과녁을 모두 부순 플레이어가 승리하게 됩니다.\n플레이어는 enter를 누를 시 화살을 발사할 수 있고 space+enter를 누르면 다음 모양의 화살로 변경할 수 있습니다.");
+                System.out.println();
+                System.out.println("Enter를 누르면 게임이 시작 됩니다");
+            }
+        };
+        b1.addActionListener(l1);
+        setVisible(true);
+    }
+}
+
 public class game3modify {
     public static void main(String[] args) {
         Scanner Scan = new Scanner(System.in);
+        Detail2 d = new Detail2();
+        game3 a = new game3();
+        a.start();
         for (int kkk = 0; kkk < 3; kkk++) {
-            game3 a = new game3();
+            String nextment = Scan.nextLine();
+            System.out.println("게임 " + (kkk+1) + " 시작합니다.");
+            nextment = Scan.nextLine();
             a.boxmaking();
-            a.start();
             outwhile : while (true) {
                 a.playercnt = 0;
                 for (int i = 0; i < 5; i++) {
